bde0266596e42a982c4844fc1b8ef06ad05dd6ac
VicV <vic@taplytics.com>
2019-08-07 18:11:31 -0400

    Bad ref

--- utils/index.js
+++ utils/index.js
@@ -22,8 +22,6 @@ var functions = {
   swapJiraAccountIdWithJiraName: function(commentBody, userMentions, user) {
     userMentions.forEach(userMention => {
       user.getByJiraUsername(userMention).then((thisUser, index) => {
-        console.log ("mention: "+userMention)
-        console.log ("shortName: "+jiraShortName);
         commentBody = commentBody.replace(userMention, thisUser.jiraShortName)
       })
     })
